68d7d2af3ad5f1cbbbe1ebf726881421e74a7f30
huando22222 <huando22222@gmail.com>
2023-05-28 01:13:43 +0700

    update addGoods admin right to upload new goods

--- client/src/components/Purchased.js
+++ client/src/components/Purchased.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
 import './Purchased.css';
 import PurchasedTab from './purchasedTab/PurchasedTab';
 import AddGoods from './purchasedTab/AddGoods';
+import { MyContext } from './context/MyContext';
+import { useState , useEffect,useContext  } from 'react';
 function Purchased(){
     const [key, setKey] = useState('purchased');
-
+    const { user} = useContext(MyContext);
     return (
         <Tabs
         id="controlled-tab-example"
@@ -18,9 +19,11 @@ function Purchased(){
             <Tab eventKey="purchased" title="Lịch sử mua hàng">
                 <PurchasedTab/>
             </Tab>
-            <Tab eventKey="addGoods" title="thêm sản phẩm">
-                <AddGoods/>
-            </Tab>
+            {user && user.admin === "true" (
+                <Tab eventKey="addGoods" title="thêm sản phẩm">
+                    <AddGoods/>
+                </Tab>
+            )}
             <Tab eventKey="contact" title="Contact" disabled>
                 Tab content for Contact
             </Tab>
